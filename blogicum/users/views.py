e8e04593a086eeb85bfd6df87a8732f850517164
nepa27 <nepalek@mail.ru>
2024-03-03 19:07:45 +0300

    Ready

--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db.models import Count
 from django.views.generic import CreateView, UpdateView, ListView
 from django.shortcuts import get_object_or_404, reverse
 from django.urls import reverse_lazy
